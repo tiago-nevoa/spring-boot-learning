be0a4a28448dad54de75f1d67d2001da650a23d5
Tiago M. F. Nevoa <tiagonevoa@gmail.com>
2023-05-22 15:21:05 +0100

    repository with spring data

---
+++ learning-spring/src/main/java/com/tn/spring/learningspring/data/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.tn.spring.learningspring.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room,Long>{
+}
