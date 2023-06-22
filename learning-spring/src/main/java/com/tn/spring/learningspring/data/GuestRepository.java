fdf2706df452170284304df3dd5d57617d3c4564
Tiago M. F. Nevoa <tiago.nevoa@sap.com>
2023-06-22 15:49:28 +0100

    spring boot data working with JDBC

--- learning-spring/src/main/java/com/tn/spring/learningspring/data/GuestRepository.java
+++ learning-spring/src/main/java/com/tn/spring/learningspring/data/GuestRepository.java
@@ -1,9 +1,9 @@
 package com.tn.spring.learningspring.data;
 
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoomRepository extends CrudRepository<Room,Long>{
+public interface GuestRepository extends CrudRepository<Guest, Long>{
 }
+
--- learning-spring/src/main/java/com/tn/spring/learningspring/data/ReservationRepository.java
+++ learning-spring/src/main/java/com/tn/spring/learningspring/data/ReservationRepository.java
@@ -1,9 +1,8 @@
 package com.tn.spring.learningspring.data;
 
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoomRepository extends CrudRepository<Room,Long>{
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
 }
