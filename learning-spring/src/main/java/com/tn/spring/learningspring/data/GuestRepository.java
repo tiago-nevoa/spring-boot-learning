4df5cc3ebf3d1b5c2b987014510e5c3c69f64e26
Tiago M. F. Nevoa <tiago.nevoa@sap.com>
2023-06-22 16:07:52 +0100

    Challenge update

--- learning-spring/src/main/java/com/tn/spring/learningspring/data/GuestRepository.java
+++ learning-spring/src/main/java/com/tn/spring/learningspring/data/GuestRepository.java
@@ -1,9 +1,10 @@
 package com.tn.spring.learningspring.data;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GuestRepository extends CrudRepository<Guest, Long>{
+public interface GuestRepository extends CrudRepository<Guest, Long> {
 }
 
--- learning-spring/src/main/java/com/tn/spring/learningspring/data/ReservationRepository.java
+++ learning-spring/src/main/java/com/tn/spring/learningspring/data/ReservationRepository.java
@@ -1,8 +1,11 @@
 package com.tn.spring.learningspring.data;
 
+import java.sql.Date;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    //Iterable<Reservation> findReservationByReservationDate(Date date);
 }
